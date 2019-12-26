f38992d4c8bd9ba13c61bf7810326bdb64e8140e
rodrigogomesrc <rocha.rodrigogomes@gmail.com>
2019-12-26 14:58:26 -0300

    error correction when renamed

--- ftdhandler.py
+++ ftdhandler.py
@@ -57,7 +57,7 @@ class FTDHandler(object):
 	def __load_english_stopwords(self):
 
 
-		english_file = open("./TxtStringify/english_stopwords.txt", "r")
+		english_file = open("./FTDHandler/english_stopwords.txt", "r")
 		english_lines = english_file.readlines()
 
 		for line in english_lines:
@@ -69,7 +69,7 @@ class FTDHandler(object):
 
 	def __load_portuguese_stopwords(self):
 
-		portuguese_file = open("./TxtStringify/portuguese_stopwords.txt", "r")
+		portuguese_file = open("./FTDHandler/portuguese_stopwords.txt", "r")
 		portuguese_lines = portuguese_file.readlines()
 
 		for line in portuguese_lines:
@@ -246,4 +246,4 @@ class FTDHandler(object):
 
 		return
 
-txtstringify = TxtStringify()
+ftdhandler = FTDHandler()
